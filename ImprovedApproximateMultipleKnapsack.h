2d5280bc9d369ce73a9407a461db4b199ed19758
Bastian Holst <bastianholst@gmx.de>
2011-03-14 13:02:08 +0100

    Adding comments for most function of ImprovedApproximateMultipleKnapsack.

--- ImprovedApproximateMultipleKnapsack.h
+++ ImprovedApproximateMultipleKnapsack.h
@@ -18,9 +18,26 @@ public:
 private:
     virtual void recalculateValues();
     
+    /**
+     * Create the groups of items.
+     * @param approximateMaximum is the approximated maximum.
+     */
     void groupItems(int approximateMaximum);
     
+    /**
+     * Generates the next possible subset of all large items.
+     * 
+     * @param highProfitSubset A bool array which is at least m_firstMediumProfitOrderIndex long
+     *                         for which "highProfitSubset[i] == true" is equal to "item i is in the subset".
+     *                         It will contain the next subset at return.
+     * @param subsetSize The number of items in the subset highProfitSubset.
+     * @param profit The profit for the subset highProfitSubset.
+     */
     bool nextHighProfitSubset(bool *highProfitSubset, int *subsetSize, int *profit);
+    
+    /**
+     * Creating a printable string from the subset @p highProfitSubset.
+     */
     QString highProfitSubsetToString(bool *highProfitSubset);
     
     /**
@@ -37,6 +54,21 @@ private:
      * @return Returns true, if there is a feasible assignment.
      */
     bool firstHighProfitSubsetAssignment(bool *highProfitSubset, int *assignment, int *remainingCapacity);
+    /**
+     * Generates the next possible assignment of all items in subset @p highProfitSubset to the bins.
+     * 
+     * @param highProfitSubset A bool array which is at least m_firstMediumProfitOrderIndex long
+     *                         for which "highProfitSubset[i] == true" is equal to "item i is in the subset".
+     * @param assignment An int array which is at least items().size() long.
+     *                   It has to be an assignment of all items in the subset that does not have to be
+     *                   feasible.
+     *                   After returning this contains the assignment of all items in the subset to the bins.
+     *                   This assignment is feasible.
+     * @param remainingCapacity An int array which is at least sizes().size() long.
+     *                          It has to contain the remaining capacities of all bins for the assignment
+     *                          assignment.
+     *                          After returning this contains the remaining capacities for each bin.
+     */
     bool nextHighProfitSubsetAssignment(bool *highProfitSubset, int *assignment, int *remainingCapacity);
     
     int m_numberOfBins;
